cf1df748918765e4b507de31f83eb0030e6d277c
viggo95yoda <43821187+viggo95yoda@users.noreply.github.com>
2018-11-15 12:55:36 +0100

    Change line 1, 2 and 3

---
+++ logger_sv.lua
@@ -0,0 +1,43 @@
+local logs = "https://discordapp.com/api/webhooks/505830290758041610/c3rA85ifJeq26dzoqHRMHn0kouD3smRr-q8nQ_8bez-DawIHf1eUFxsq_GmBZYNhM2XX"
+local communityname = "Big Yoda"
+local communtiylogo = "https://i.imgur.com/e8VsdLL.jpg" --Must end with .png
+
+AddEventHandler('playerConnecting', function()
+local name = GetPlayerName(source)
+local ip = GetPlayerEndpoint(source)
+local ping = GetPlayerPing(source)
+local steamhex = GetPlayerIdentifier(source)
+local connect = {
+        {
+            ["color"] = "8663711",
+            ["title"] = "Player Connected to Server #1",
+            ["description"] = "Player: **"..name.."**\nIP: **"..ip.."**\n Steam Hex: **"..steamhex.."**",
+	        ["footer"] = {
+                ["text"] = communityname,
+                ["icon_url"] = communtiylogo,
+            },
+        }
+    }
+
+PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Big Yoda Server Logger", embeds = connect}), { ['Content-Type'] = 'application/json' })
+end)
+
+AddEventHandler('playerDropped', function(reason)
+local name = GetPlayerName(source)
+local ip = GetPlayerEndpoint(source)
+local ping = GetPlayerPing(source)
+local steamhex = GetPlayerIdentifier(source)
+local disconnect = {
+        {
+            ["color"] = "8663711",
+            ["title"] = "Player Disconnected from Server #1",
+            ["description"] = "Player: **"..name.."** \nReason: **"..reason.."**\nIP: **"..ip.."**\n Steam Hex: **"..steamhex.."**",
+	        ["footer"] = {
+                ["text"] = communityname,
+                ["icon_url"] = communtiylogo,
+            },
+        }
+    }
+
+    PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Big Yoda Server Logger", embeds = disconnect}), { ['Content-Type'] = 'application/json' })
+end)
